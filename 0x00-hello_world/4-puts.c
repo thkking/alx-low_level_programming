edb1978b34473cfce4ec9958f61810ff7bcf3c01
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-16 05:33:01 -0700

    Using put command task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stout.
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	put("\"Programming is like builing a multilingual puzzle");
+	return(0);
+}

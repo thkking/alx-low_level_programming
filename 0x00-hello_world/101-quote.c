14f75e9b0d0f45ab7d9c031da84d1e4c7740ac6f
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-17 13:02:45 -0700

    101-quote.c Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: 1 
+ * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piecen of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }

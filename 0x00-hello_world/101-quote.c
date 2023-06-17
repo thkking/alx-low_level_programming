82b4484ee3d53d0496246d6f16e6e507b897209e
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-17 12:50:15 -0700

    101-quote.c, Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: 1 
+ */
+int main(void)
+{
+	write(2, "and that piecen of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
+}

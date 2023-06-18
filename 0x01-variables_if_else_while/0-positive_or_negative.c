59c1d8c0546a6f46e053922dc12f022bc0e7aeb4
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-18 15:57:07 -0700

    0-positive_or_negative.c.Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Determine if a random number is positive, negative or zero.
+ *
+ * Return:0 if success.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand () - RAND_MAX / 2;
+	/* your code goes there */
+	if(n>0)
+	{
+		printf("%d is %s\n", n, "is positive");
+	}
+	else if(n==0)
+	{
+		printf("%d is %s\n", n, "is zero");
+	}
+	else
+	{
+		printf("%d is %s\n", n, "is negative");
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,4 @@
+This directory is about arithmetic operators, relational operators and logical operators how to use them.
+What are boolean operators, which values are considered TRUE and FALSE in C.
+How to use the if, if....else statements, comments.
+How to declare variables of types char, int, unsigned int, how to print char, int, unsigned int, witn printf. How to use while loop. The purpose of the gcc flags -m32 and -m64. 

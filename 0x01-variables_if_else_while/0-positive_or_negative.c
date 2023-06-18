427a266d8b3aa12206381f48259da4e4f5272ac3
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-18 16:37:34 -0700

    0-positive_or_negative.c. Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
 {
-printf("%d is %s\n", n, "is positive");
+printf("%d is %s\n", n, "positive");
 }
 else if (n < 0)
 {
-printf("%d is %s\n", n, "is negative");
+printf("%d is %s\n", n, "negative");
 }
 else
 {
-printf("%d is %s\n", n, "is zero");
+printf("%d is %s\n", n, "zero");
 }
 return (0);
 }

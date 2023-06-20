20dcad4ea63223e9fd22ffacf7e0e387b0d17504
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-19 20:45:15 -0700

    4-print_alphabt.c Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - returns aphabets both except eand q
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		if ((ch != 'e') && (ch != 'q'))
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}

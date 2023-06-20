5dec18d08020529ff6c2c53634f00edccbabb425
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-19 23:19:51 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,14 +16,12 @@ for (hundreds = '0'; hundreds <= '9'; hundreds++)
 for (tens = '0'; tens <= '9'; tens++)
 {
 for (ones = '0'; ones <= '9'; ones++)
-if (!((ones == tens) || (tens == hundreds) || 
-(tens > ones) || (hundreds > tens)))
+if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
 {
 putchar(hundreds);
 putchar(tens);
 putchar(ones);
-if (!(ones == '9' && hundreds == '7' && 
-tens == '8'))
+if (!(ones == '9' && hundreds == '7' && tens == '8'))
 {
 putchar(',');
 putchar(' ');

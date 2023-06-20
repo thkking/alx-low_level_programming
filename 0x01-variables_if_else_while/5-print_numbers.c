9b5f5dc1b308424b26a11de02302452543aaca95
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-19 21:16:12 -0700

    5-print_numbers.c Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main return single digit numbers of base 10
  *
  * Retuen: 0 if success
  */
+
 int main(void)
 {
+
 	int num;
+
 	while (num < 10)
 	{
 		printf("%d", num);

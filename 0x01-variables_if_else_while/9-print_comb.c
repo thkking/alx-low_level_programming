cdf9ec5677db42c4bbc83dad318b496bd38dad7a
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-19 22:06:36 -0700

    9-print_comb.c Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible combinations
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(',');
+		}
+		++i;
+	}
+	putchar('\n');
+	return (0);
+}

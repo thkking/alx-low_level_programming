0a7e5697b0f01b325862fa24503d3e19dfa61059
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-19 22:42:49 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,7 +17,7 @@ for (tens = '0'; tens <= '9'; tens++)
 for (ones = '0'; ones <= '9'; ones++)
 {
 if (!((ones == tens) || (tens > ones)))
-{	
+{
 putchar(tens);
 putchar(ones);
 if (!(ones == '9' && tens == '8'))

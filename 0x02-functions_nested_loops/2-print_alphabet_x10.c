da0df3997728b76f40cd1eb02570303c4ba368ec
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-21 22:56:42 -0700

    Task 2 2-print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Prints the alphabets in lowercase
- *
- * Return: 0 if success
+ * print_alphabet_x10 -  Make alphabet x10 times
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	int j;
-
-	for (j = 1; i <= 10; i++)
+	char c;
+	int i = 0;
+	
+	while (i <= 9)
 	{
-		for (j = 97; j <= 122; i++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
-			_putchar(j);
+			_putchar(c);
 		}
 		_putchar('\n');
+		i++;
 	}
 }

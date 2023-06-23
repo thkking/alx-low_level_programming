5a7ef26d73221a35adf073be5327fa81a9b9918f
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-23 03:51:59 -0700

    101-natural.c task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints the sumof all multiples of 3 or 5 upto 1024
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i = 0;
+	int z = 0;
+
+	while(i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+		i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}

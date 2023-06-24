636f68d6b525a40cc0b9b80ee27303fee7f1910c
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-24 16:53:14 -0700

    1-main.c 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,11 +11,11 @@ int main(void)
 
 	printf("infinite loop incoming :(\n");
 	i = 0;
-	while (i < 10)
+/*	while (i < 10)
 	{
 		putchar(i);
 	}
-
+*/
 	print("Infinite loop avoided! \\o/\n");
 
 	return (0);

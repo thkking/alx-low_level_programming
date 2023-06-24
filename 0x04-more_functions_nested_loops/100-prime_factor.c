bf7b42b1b50df137b82b13490a628162030c4549
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-24 01:06:09 -0700

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the largest prime factor
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	long x;
+	long maxf;
+	double number = 612852475143
+
+		for (x = 1; x <= square; x++)
+		{
+			if (number % x == 0)
+			{
+				maxf = number / x;
+			}
+		}
+	printf("%d\n", maxf);
+	return (0);
+}

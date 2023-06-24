735cb2cfd6a23440209daffb3a8e844fbc584198
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-23 22:01:41 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,16 +7,19 @@
 
 void print_most_numbers(void)
 {
-	int x = 0;
-	for (; x <= 9; x++)
-	{if (x == 2 || x == 4)
+int x = 0;
+
+for (; x <= 9; x++)
+{
+if (x == 2 || x == 4)
+	
 		{
-			continue;
-		}
-		else
-		{
-			_putchar(x + '0');
-		}
-	}
-	_putchar('\n');
+continue;
+}
+else
+{
+_putchar(x + '0');
+}
+}
+_putchar('\n');
 }

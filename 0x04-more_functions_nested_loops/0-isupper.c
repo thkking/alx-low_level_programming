68a1f45b15c8e68a70f38f4916541cfafe01663b
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-06-23 17:44:28 -0700

    0-isupper .c Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 /**
  * _isupper - Checks if a is uppercase
  * @x: The number to be checked
- * Return: 1 for uppercase or 0 for anything else
+ * Return: 1 for uppercase or 0 for any else
  */
 
 int _isupper(int x)
 {
-	if (x >= 65 && x <= 90)
-	{
-		return (1);
-	}
-	return (0);
-} 
+if (x >= 65 && x <= 90)
+{
+return (1);
+}
+return (0);
+}

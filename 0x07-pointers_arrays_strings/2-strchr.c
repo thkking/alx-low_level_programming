6afad2f749d5120891818b6cba8cc2f4aa3d5a8a
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-07-03 22:28:45 -0700

    2-strchr.c Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - Entery point
+ * @s: input
+ * @c: input
+ * Return: 0 if success
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}

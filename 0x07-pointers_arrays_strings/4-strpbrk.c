c3ba046810796d751a68fe1a7d323300cfebda7b
Theodosius Koranteng King <thkorantengking@gmail.com>
2023-07-04 05:33:49 -0700

    4-strpbrk.c Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,16 +8,16 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int k;
-
-	while (*s)
-	{
-		for (k = 0; accept[k]; k++)
-		{
-			if (*s == accept[k])
-				return (s);
-		}
-		s++;
-	}
-	return ("\0");
+while (*s)
+{
+char *p = accept;
+while (*p)
+{
+if (*s == *p)
+return s;
+p++;
+}
+s++;
+}
+return (0);
 }
